3a3a6e61338db21486f4023f387fa3ec43909eca
Lucas Rosario <lucasrosario@gmail.com>
2020-07-15 02:10:50 -0300

    commit

---
+++ target/run/jaffle_shop/models/staging/stg_customers.sql
@@ -0,0 +1,11 @@
+
+
+  create or replace view `bigquery-dbt`.`dbt_lucas`.`stg_customers`
+  OPTIONS()
+  as select
+    id as customer_id,
+    first_name,
+    last_name
+
+from `dbt-tutorial`.jaffle_shop.customers;
+
---
+++ target/run/jaffle_shop/models/staging/stg_orders.sql
@@ -0,0 +1,12 @@
+
+
+  create or replace view `bigquery-dbt`.`dbt_lucas`.`stg_orders`
+  OPTIONS()
+  as select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+
+from `dbt-tutorial`.jaffle_shop.orders;
+
